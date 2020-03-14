2768d9e729f367ddab68fa5b438b2bdee7f61cb8
Pedro Rodrigues <csixteen@protonmail.com>
2020-03-14 22:24:12 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# rs-bff
+
+I don't know Rust, so I've decided to make a port of my [original](https://github.com/csixteen/py-bff) Python Brainfuck interpreter to Rust. I guess this sort of became my toy project for when I want to solidify some concepts while learning a new language. I've done the same for [Go](https://github.com/csixteen/go-bff) and I'll probably do the same for other languages.
+
+---
+
+I'm still pretty n00b in Rust and I'm currently still reading the [Rust book](https://doc.rust-lang.org/book/), which is pretty awesome btw. So apologies in advance if the code sucks. If you have any suggestions for improving the code, feel free to open a PR :D
---
+++ src/main.rs
@@ -0,0 +1,151 @@
+extern crate termios;
+
+use std::char;
+use std::collections::VecDeque;
+use std::convert::TryInto;
+use std::env;
+use std::fs;
+use std::io::{self, Read, Write};
+use std::process;
+
+use termios::{Termios, TCSANOW, ECHO, ICANON, tcsetattr};
+
+
+// --------------------------------------------------
+// Helpers
+
+// Reads a single char from stdin without the need for the user to
+// hit "Enter".
+// https://stackoverflow.com/questions/26321592/how-can-i-read-one-character-from-stdin-without-having-to-hit-enter
+fn getch() -> u8 {
+    let stdin = 0;
+    let termios = Termios::from_fd(stdin).unwrap();
+    let mut new_termios = termios.clone();
+
+    new_termios.c_lflag &= !(ICANON | ECHO);
+    tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
+    let stdout = io::stdout();
+    let mut reader = io::stdin();
+    let mut buffer = [0;1];  // read exactly one byte
+    stdout.lock().flush().unwrap();
+    reader.read_exact(&mut buffer).unwrap();
+    tcsetattr(stdin, TCSANOW, & termios).unwrap();
+
+    buffer[0]
+}
+
+
+// --------------------------------------------------
+// `Program` struct and implementation
+
+struct Program<'a> {
+    code: &'a Vec<char>,     // Brainfuck code
+    ip: usize,               // Instruction pointer
+    cursor: usize,           // Memory cursor
+    stack: VecDeque<usize>,  // Program stack
+    memory: Vec<u8>,         // Memory
+}
+
+impl<'a> Program<'a> {
+    fn new(code: &Vec<char>) -> Program {
+        Program {
+            code: code,
+            ip: 0,
+            cursor: 0,
+            stack: VecDeque::new(),
+            memory: vec![0; 30000],
+        }
+    }
+
+    fn execute_instruction(&mut self) {
+        let mut offset: i32 = 1;
+
+        match self.code[self.ip] {
+            '<' => self.cursor -= 1,
+            '>' => self.cursor += 1,
+            '+' => self.memory[self.cursor] += 1,
+            '-' => self.memory[self.cursor] -= 1,
+            '.' => {
+                let c = char::from_u32(self.memory[self.cursor].into()).unwrap();
+                print!("{}", c);
+            }
+            ',' => self.memory[self.cursor] = getch(),
+            '[' => {
+                if self.memory[self.cursor] > 0 {
+                    self.stack.push_front(self.ip);
+                } else {
+                    self.stack.pop_front();
+                    offset = self.code
+                        .iter()
+                        .skip(self.ip + 1)
+                        .position(|&x| x == ']')
+                        .unwrap()
+                        .try_into()
+                        .unwrap();
+                }
+            }
+            ']' => {
+                let v = self.stack.pop_front();
+                if self.memory[self.cursor] > 0 {
+                    offset = (v.unwrap() as i32) - (self.ip as i32);
+                }
+            }
+            _ => (),
+        }
+
+        // I'm still a bit unsure how to properly solve this case.
+        // If I just leave as `self.ip += offset as usize` then
+        // I get a runtime error saying that I'm attempting to subtract
+        // with overflow.
+        if offset < 0 {
+            self.ip -= offset.abs() as usize;
+        } else {
+            self.ip += offset as usize;
+        }
+    }
+
+    fn execute(&mut self) {
+        while self.ip < self.code.len() {
+            self.execute_instruction();
+        }
+    }
+}
+
+
+// ------------------------------------------------------
+
+
+// Reads the contents of a source file and returns
+// a Vector with the chars that only represent valid
+// Brainfuck operators: <>+-,.[]
+fn load_code(file_name: &str) -> Vec<char> {
+    let contents = fs::read_to_string(file_name)
+        .expect("Couldn't read from file");
+
+    contents
+        .chars()
+        .filter(|c| {
+            match c {
+                '<' | '>' | '+' | '-' | ',' | '.' | '[' | ']' => true,
+                _ => false,
+            }
+        })
+        .collect::<Vec<char>>()
+}
+
+
+// -------------------------------------------------------
+
+
+// Entry point
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: <prog> <filename.bf>");
+        
+        process::exit(1);
+    }
+
+    Program::new(&load_code(&args[1])).execute();
+}
